aa67c6786a14ba1edf56315a8efa15170bd7d069
immeëmosol <will+developer@willfris.nl>
2019-02-25 22:49:06 +0100

    Update Usgo.php

--- src/Usgo.php
+++ src/Usgo.php
@@ -4,22 +4,8 @@ namespace AbuseIO\Parsers;
 
 use AbuseIO\Models\Incident;
 
-/**
- * Class Usgo
- * @package AbuseIO\Parsers
- */
-class Usgo extends Parser
+class Usgo extends ParserBase
 {
-    /**
-     * Create a new Usgo instance
-     *
-     * @param \PhpMimeMailParser\Parser $parsedMail phpMimeParser object
-     * @param array $arfMail array with ARF detected results
-     */
-    public function __construct($parsedMail, $arfMail)
-    {
-        parent::__construct($parsedMail, $arfMail, $this);
-    }
 
     /**
      * Parse attachments
@@ -28,38 +14,43 @@ class Usgo extends Parser
      */
     public function parse()
     {
-        if (($this->arfMail !== false) &&
-            (preg_match_all('/([\w\-]+): (.*)[ ]*\r?\n/', $this->arfMail['report'] . PHP_EOL, $matches))
-        ) {
-            $report = array_combine($matches[1], $matches[2]);
-            $this->feedName = 'default';
-
-            // If feed is known and enabled, validate data and save report
-            if ($this->isKnownFeed() && $this->isEnabledFeed()) {
-                // Sanity check
-                if ($this->hasRequiredFields($report) === true) {
-                    // incident has all requirements met, filter and add!
-                    $report = $this->applyFilters($report);
-
-                    $report['evidence'] = $this->arfMail['evidence'];
-
-                    $incident = new Incident();
-                    $incident->source      = config("{$this->configBase}.parser.name");
-                    $incident->source_id   = false;
-                    $incident->ip          = $report['Source-IP'];
-                    $incident->domain      = false;
-                    $incident->class       = config("{$this->configBase}.feeds.{$this->feedName}.class");
-                    $incident->type        = config("{$this->configBase}.feeds.{$this->feedName}.type");
-                    $incident->timestamp   = strtotime($report['Received-Date']);
-                    $incident->information = json_encode($report);
-
-                    $this->incidents[] = $incident;
-                }
-            }
-        } else {
+        if ($this->arfMail === false) {
             $this->warningCount++;
+            return $this->success();
         }
+        $matched = preg_match_all('/([\w\-]+): (.*)[ ]*\r?\n/', $this->arfMail['report'] . PHP_EOL, $matches);
+        if (!$matched) {
+            $this->warningCount++;
+            return $this->success();
+        }
+        $report = array_combine($matches[1], $matches[2]);
+        $this->feedName = 'default';
 
+        // If feed is known and enabled, validate data and save report
+        $kae = $this->isKnownFeed() && $this->isEnabledFeed();
+        if ($kae) {
+            return $this->success();
+        }
+        // Sanity check
+        if ($this->hasRequiredFields($report) !== true) {
+            return $this->success();
+        }
+        // incident has all requirements met, filter and add!
+        $report = $this->applyFilters($report);
+
+        $report['evidence'] = $this->arfMail['evidence'];
+
+        $incident = new Incident();
+        $incident->source      = config("{$this->configBase}.parser.name");
+        $incident->source_id   = false;
+        $incident->ip          = $report['Source-IP'];
+        $incident->domain      = false;
+        $incident->class       = config("{$this->configBase}.feeds.{$this->feedName}.class");
+        $incident->type        = config("{$this->configBase}.feeds.{$this->feedName}.type");
+        $incident->timestamp   = strtotime($report['Received-Date']);
+        $incident->information = json_encode($report);
+
+        $this->incidents[] = $incident;
         return $this->success();
     }
 }
